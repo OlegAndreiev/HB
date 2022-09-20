806f45bc8dfb96b22d566479cc1b9e4762486d12
Oleg Andreiev <mr.olegandreev@gmail.com>
2022-09-20 21:24:17 +0300

    Ooo

--- JS/script.js
+++ JS/script.js
@@ -1,10 +1,17 @@
 const button = document.querySelector('button');
 const baloonsAction = document.querySelector('.baloons-image');
 const titleImage = document.querySelector('.title-image');
+const petardAction = document.querySelector('.petard-image');
+const petardRotateAction = document.querySelector('.petard-image-rotated');
+const tapeImageRight = document.querySelector('.tape-image-right');
+const tapeImageLeft = document.querySelector('.tape-image-left');
 
 button.addEventListener('click', () => {
   baloonsAction.classList.add('action');
-
   titleImage.classList.add('action');
+  petardAction.classList.add('action');
+  petardRotateAction.classList.add('action');
+  tapeImageRight.classList.add('action');
+  tapeImageLeft.classList.add('action');
 });
 console.dir(baloonsAction);
