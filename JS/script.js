d90c5062aa2e7aaf56b2c965a9ace6e8b560e50c
Oleg Andreiev <mr.olegandreev@gmail.com>
2022-09-20 22:22:36 +0300

    qqq

--- JS/script.js
+++ JS/script.js
@@ -5,13 +5,23 @@ const petardAction = document.querySelector('.petard-image');
 const petardRotateAction = document.querySelector('.petard-image-rotated');
 const tapeImageRight = document.querySelector('.tape-image-right');
 const tapeImageLeft = document.querySelector('.tape-image-left');
+const bgImageAction = document.querySelector('.bg-image');
 
 button.addEventListener('click', () => {
+  button.classList.add('action');
   baloonsAction.classList.add('action');
   titleImage.classList.add('action');
   petardAction.classList.add('action');
   petardRotateAction.classList.add('action');
   tapeImageRight.classList.add('action');
   tapeImageLeft.classList.add('action');
+  bgImageAction.classList.add('action');
+  sound();
 });
-console.dir(baloonsAction);
+
+function sound() {
+  var audio = new Audio(); // Создаём новый элемент Audio
+  audio.src = './Audio/happy-birthday-to-you-medlennaya.mp3'; // Указываем путь к звуку "клика"
+  audio.autoplay = true; // Автоматически запускаем
+  // audio.loop = true;
+}
